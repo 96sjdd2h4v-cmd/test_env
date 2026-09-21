b9b4e9c34115d3cfae86396d7fb72091baeda603
Beau Frans <beaufrans@Beaus-MacBook-Pro.local>
2026-09-21 10:44:38 +0200

    v9

--- deploy/drupal_vm/startup_drupal.sh
+++ deploy/drupal_vm/startup_drupal.sh
@@ -2,7 +2,14 @@
 REPO_DIR="/home/beaufrans/drupal"
 REPO_URL="https://${PAT_TOKEN}@github.com/96sjdd2h4v-cmd/test_env.git"
 
-# 1. Repo ophalen
+mkdir -p ~/drupal/sites/default/files \
+         ~/drupal/sites/multisite-a.local/files \
+         ~/drupal/sites/multisite-b.local/files \
+         ~/drupal/sites/multisite-c.local/files \
+         ~/drupal/sites/multisite-d.local/files \
+         ~/drupal/sites/domain-b.local/files
+
+#Repo ophalen
 if [ ! -d "$REPO_DIR" ]; then
   git clone "$REPO_URL" "$REPO_DIR"
   chown -R beaufrans:beaufrans "$REPO_DIR"
@@ -10,9 +17,9 @@ else
   cd "$REPO_DIR" && git pull
 fi
 
-# 2. Authenticeer Docker bij Artifact Registry
+#Authenticeer Docker bij Artifact Registry
 gcloud auth configure-docker europe-west1-docker.pkg.dev --quiet
 
-# 3. Ga naar de juiste map en start Docker Compose
+#Ga naar de juiste map en start Docker Compose
 cd "$REPO_DIR/deploy/drupal_vm"
 docker compose up -d
